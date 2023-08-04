8e31fb79b89cb41ef2599a181066186bba2dbec2
Nishan45 <114748319+Nishan45@users.noreply.github.com>
2023-08-04 07:57:41 +0530

    Add files via upload

---
+++ Other.js
@@ -0,0 +1,41 @@
+const mongoose=require('mongoose')
+
+const OtherSchema=new mongoose.Schema({
+    user_id:{
+        type:String, 
+        required:true
+    },
+    category:{
+        type:String,
+        
+    },
+    title:String,
+    body:{
+        type:String,
+        required:true,
+    },
+    author:{
+        type:String,
+    },
+    
+    profImg:{
+        type:String,
+    },
+    name:{
+        type:String,
+        required:true
+    },
+    
+    likes:{
+        type:Map,
+        of:Boolean
+    },
+    comments:{
+        type:Array,
+        default:[]
+    },
+
+}, {timestamps:true}
+);
+    
+module.exports=mongoose.model('Other',OtherSchema);
---
+++ PostImg.js
@@ -0,0 +1,35 @@
+const mongoose=require('mongoose');
+
+
+const PostSchema=new mongoose.Schema({
+    user_id:{
+        type:String, 
+        required:true
+    },
+    image:{
+        type:String,
+        required:true
+    },
+    profImg:{
+        type:String,
+        required:true
+    },
+    name:{
+        type:String,
+        required:true
+    },
+    discription:String,
+    
+    likes:{
+        type:Map,
+        of:Boolean
+    },
+    comments:{
+        type:Array,
+        default:[]
+    },
+
+}, {timestamps:true}
+);
+    
+module.exports=mongoose.model('PostImg',PostSchema);
